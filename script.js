a797a24007fe3b7c38460543260ac861c58a3c91
Ricardo Blanco <cloud.tools.g81tr@slmails.com>
2025-09-20 22:09:32 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const envelopeWrapper = document.querySelector('.envelope-wrapper');
-    const envelope = document.querySelector('.envelope');
-
-    envelopeWrapper.addEventListener('click', () => {
-        envelope.classList.toggle('open');
-    });
-});
